8e5d242ae382d1738fa751aa0f353ea74d8457d4
rustam-ahmadov <ahmadov.rustam114@gmail.com>
2023-12-28 14:42:43 +0400

    feat: logger setup

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log/slog"
+	"os"
 	"url-shortener/internal/config"
 )
 
@@ -16,13 +18,25 @@ func main() {
 	cfg := config.MustLoad()
 	fmt.Print(cfg)
 
+	log, err := setupLogger(cfg.Env)
+	if err != nil{
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+	log.Info("starting url-shortener")
+	log.Debug("debug messages are enabled")
+	
+
 }
 
-func setupLogger(env string) {
-	var log *slog.Logger
-
+func setupLogger(env string) (*slog.Logger, error) {
 	switch env {
 	case envLocal:
-
+		return slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})), nil
+	case envDev:
+		return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})), nil
+	case envProd:
+		return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})), nil
 	}
+	return nil, errors.New("logger has not been set up")
 }
