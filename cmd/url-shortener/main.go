d0dfeddedb3729473a5e026c20d9d96ff0c58da9
rustam-ahmadov <ahmadov.rustam114@gmail.com>
2024-01-03 14:05:32 +0400

    feat: change collection name from url to urls

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -17,17 +17,20 @@ const (
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Print(cfg)
 
 	log, err := setupLogger(cfg.Env)
 	if err != nil {
-		fmt.Print(err.Error())
+		fmt.Printf("err occured while initializing logger: %s", err.Error())
 		os.Exit(1)
 	}
 
-	storage := mongo_storage.NewStorage(cfg.Storage_Path, cfg.Storage_Name)
-	storage.Log("url-shortener has been started",slog.LevelInfo)
-
+	storage, err := mongo_storage.NewStorage(cfg.Storage_Path, cfg.Storage_Name)
+	if err != nil {
+		fmt.Printf("err occured while initializing storage: %s", err.Error())
+		os.Exit(1)
+	}
+	log.Info("url-shortener has been started")
+	storage.Log("url-shortener has been started", slog.LevelInfo)
 	log.Debug("debug messages are enabled")
 }
 
